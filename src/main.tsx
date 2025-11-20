40f2b56709cb6d0679a70884f603c3fba2a14286
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2025-11-20 10:07:22 -0300

    fix theme

--- src/main.tsx
+++ src/main.tsx
@@ -8,20 +8,3 @@ createRoot(document.getElementById('root')!).render(
     <Router />
   </StrictMode>,
 )
-
-
-const themeColorMeta = document.querySelector('meta[name="theme-color"]');
-
-function updateThemeColor() {
-  if (globalThis.matchMedia('(prefers-color-scheme: dark)').matches) {
-    themeColorMeta!.setAttribute('content', '#000000'); // Dark mode theme color
-  } else {
-    themeColorMeta!.setAttribute('content', '#ffffff'); // Light mode theme color
-  }
-}
-
-// Call on page load
-updateThemeColor();
-
-// Listen for changes in preferred color scheme
-globalThis.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateThemeColor);
